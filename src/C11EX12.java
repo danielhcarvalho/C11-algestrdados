02134aaae4309251d73a898ff7f95f7a0771bd4d
Daniel Henrique de Carvalho <danhcarv.alves@gmail.com>
2024-05-03 11:56:44 -0300

    EXERCÍCIO C11EX12

--- src/C11EX12.java
+++ src/C11EX12.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class C11EX11
+public class C11EX12
 {
     public static void main(String[] args) 
     {
@@ -15,7 +15,11 @@ public class C11EX11
         {
             for (int j = 1; j <= i; j++) 
             {
-                System.out.print(i);   
+                System.out.print(j);
+            }
+            for (int j = i; j > 0; j--) 
+            {
+                System.out.print(j);
             }
             System.out.println();
         }
