3896cac533028f765a89472a7e59c7f811d3267b
Daniel Henrique de Carvalho <danhcarv.alves@gmail.com>
2024-05-03 11:52:02 -0300

    EXERCÍCIO C11EX11

---
+++ src/C11EX11.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class C11EX11 
+{
+    public static void main(String[] args) 
+    {
+        int num;
+        String impressão;
+
+        num = Integer.parseInt(JOptionPane.showInputDialog(null,
+        "Informe o número de repetições:",
+        "",
+        JOptionPane.QUESTION_MESSAGE));
+
+        for (int i = 1; i <= num; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(j);
+            }
+            for (int j = i; j > 0; j--) 
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
